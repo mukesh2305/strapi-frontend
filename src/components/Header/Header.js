5d300dd6e7baf890525c2f0c358083773250afa3
mukesh2305 <yogahindu1999@gmail.com>
2022-01-07 23:55:03 +0530

    front-end

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react'
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <Fragment>
+            <div className='header'>
+                <div className='leftSection'>
+                    <h1>dfdfdfdf</h1>
+                </div>
+                <div className='rightSection'>
+                    <h1>dfdfdfdfdf</h1>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header
